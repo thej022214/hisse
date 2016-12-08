9f569447ed0f6eb72e722b21f840f972391ac5b9
Brian OMeara <omeara.brian@gmail.com>
2016-12-08 11:00:37 -0500

    more min max fun

--- R/supportRegion.R
+++ R/supportRegion.R
@@ -295,8 +295,8 @@ GenerateValues <- function(par, lower, upper, scale.int, max.tries=100, expand.p
         new.vals <- rep(NA, length(par))
         for(i in sequence(length(par))) {
             examined.max[i] <- max(0.001, examined.max[i])
-            min.val <- max(lower[i], (1-scale.int)*examined.min[i])
-            max.val <- min(upper[i], (1+scale.int)*examined.max[i])
+            min.val <- min(max(lower[i], (1-scale.int)*examined.min[i]), examined.max[i]) #just in case min is greater than max
+            max.val <- max(min(upper[i], (1+scale.int)*examined.max[i]), examined.min[i])
             if(isTRUE(all.equal(min.val, max.val))) {
               min.val <- min.val * 0.9999
               max.val <- max.val * 1.0001
