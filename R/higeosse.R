e1c47baabe044497df6c7002326778a5540d79cb
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2017-10-12 10:01:13 -0500

    adding in ability to test our code against classe

--- R/higeosse.R
+++ R/higeosse.R
@@ -523,9 +523,9 @@ ParametersToPassHiGeoSSE <- function(phy, data, f, model.vec, hidden.states){
             if(data[i]==1){states[i,1]=1}
             if(data[i]==2){states[i,2]=1}
             if(data[i]==0){states[i,3]=1}
-            if(data[i]==4){states[i,10]=1}
-            if(data[i]==5){states[i,11]=1}
-            if(data[i]==3){states[i,12]=1}
+            if(data[i]==4){states[i,13]=1}
+            if(data[i]==5){states[i,14]=1}
+            if(data[i]==3){states[i,15]=1}
         }
     }
     
