29a66567d84c5e54b0bbf030078f1d975038ac42
thej022214 <jbeaulieu@nimbios.org>
2017-10-16 21:55:12 -0500

    adding odds and sods

--- R/higeosse.R
+++ R/higeosse.R
@@ -1106,7 +1106,15 @@ ParametersToPassMuSSE <- function(phy, data, f, model.vec, hidden.states){
             if(data[i]==0){states[i,c(3,6,9,12,15)]=1}
         }
     }
-    if(hidden.states == "TEST"){
+    if(hidden.states == "TEST1"){
+        states = matrix(0,Ntip(phy),15)
+        for(i in 1:Ntip(phy)){
+            if(data[i]==1){states[i,1]=1}
+            if(data[i]==2){states[i,2]=1}
+            if(data[i]==4){states[i,3]=1}
+        }
+    }
+    if(hidden.states == "TEST2"){
         states = matrix(0,Ntip(phy),15)
         for(i in 1:Ntip(phy)){
             if(data[i]==1){states[i,1]=1}
