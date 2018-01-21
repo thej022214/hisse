c52ff69fe78a47e7a29f6e5f6c3a143425b568c0
daniel <caetanods1@gmail.com>
2018-01-21 12:22:26 -0600

    Skip test for '' element if argument 'AIC.weights' is provided.

--- R/getModelAveTipRates.R
+++ R/getModelAveTipRates.R
@@ -4,17 +4,19 @@ GetModelAveTipRates <- function(x, AIC.weights=NULL){
     if( !inherits(hisse.results, what = c("list", "hisse.states", "hisse.geosse.states")) ) stop("x needs to be a list of model reconstructions or a single model reconstruction object of class 'hisse.states' or 'hisse.geosse.states'.")
 
     ## If hisse.results is a list of model reconstructions, then test if they have $aic. Return error message otherwise.
+    ## There is no need for the $aic element if AIC.weigths argument is provided.
+    ## AIC.weights is a substitute for AIC. See help page.
     if(class(hisse.results) == "list"){
-        if( !is.null( AIC.weights ) ){
+        if( is.null( AIC.weights ) ){
+            empty.aic <- sapply(hisse.results, function(x) !is.null(x$aic) )
+            if( sum( empty.aic ) != length(hisse.results) ) stop("All elements of the list need to have a '$aic' element.")
+            model.class <- sapply(hisse.results, function(x) !inherits(x, what = c("hisse.states", "hisse.geosse.states")) )
+            if( as.logical( sum( model.class ) ) ) stop("x needs to be a list of model reconstruction with class 'hisse.states' or 'hisse.geosse.states' ")
+        } else{
             if( !length(hisse.results) == length(AIC.weights) ){
                 stop( " AIC.weights needs to be NULL or a numeric vector with length equal to the number of models in 'x'. " )
             }
-            
         }
-        empty.aic <- sapply(hisse.results, function(x) !is.null(x$aic) )
-        if( sum( empty.aic ) != length(hisse.results) ) stop("All elements of the list need to have a '$aic' element.")
-        model.class <- sapply(hisse.results, function(x) !inherits(x, what = c("hisse.states", "hisse.geosse.states")) )
-        if( as.logical( sum( model.class ) ) ) stop("x needs to be a list of model reconstruction with class 'hisse.states' or 'hisse.geosse.states' ")
     }
 
     if( inherits(hisse.results, what = c("hisse.states", "hisse.geosse.states")) ){ # we have to make a list so we can run this generally
@@ -55,17 +57,19 @@ GetModelAveNodeRates <- function(x, AIC.weights=NULL){
     if( !inherits(hisse.results, what = c("list", "hisse.states", "hisse.geosse.states")) ) stop("x needs to be a list of model reconstructions or a single model reconstruction object of class 'hisse.states' or 'hisse.geosse.states'.")
 
     ## If hisse.results is a list of model reconstructions, then test if they have $aic. Return error message otherwise.
+    ## There is no need for the $aic element if AIC.weigths argument is provided.
+    ## AIC.weights is a substitute for AIC. See help page.
     if(class(hisse.results) == "list"){
-        if( !is.null( AIC.weights ) ){
+        if( is.null( AIC.weights ) ){
+            empty.aic <- sapply(hisse.results, function(x) !is.null(x$aic) )
+            if( sum( empty.aic ) != length(hisse.results) ) stop("All elements of the list need to have a '$aic' element.")
+            model.class <- sapply(hisse.results, function(x) !inherits(x, what = c("hisse.states", "hisse.geosse.states")) )
+            if( as.logical( sum( model.class ) ) ) stop("x needs to be a list of model reconstruction with class 'hisse.states' or 'hisse.geosse.states' ")
+        } else{
             if( !length(hisse.results) == length(AIC.weights) ){
                 stop( " AIC.weights needs to be NULL or a numeric vector with length equal to the number of models in 'x'. " )
             }
-            
         }
-        empty.aic <- sapply(hisse.results, function(x) !is.null(x$aic) )
-        if( sum( empty.aic ) != length(hisse.results) ) stop("All elements of the list need to have a '$aic' element.")
-        model.class <- sapply(hisse.results, function(x) !inherits(x, what = c("hisse.states", "hisse.geosse.states")) )
-        if( as.logical( sum( model.class ) ) ) stop("x needs to be a list of model reconstruction with class 'hisse.states' or 'hisse.geosse.states' ")
     }
 
     if( inherits(hisse.results, what = c("hisse.states", "hisse.geosse.states")) ){ # we have to make a list so we can run this generally
@@ -90,10 +94,10 @@ GetModelAveNodeRates <- function(x, AIC.weights=NULL){
 
     ## Objects will always be of list class here.
     if(class(hisse.results[[1]])=="hisse.states"){
-        states.internal <- ConvertManyToBinaryState(hisse.results, "node.mat", AIC.weights=AIC.weights)
+        states.internal <- ConvertManyToBinaryState(hisse.results, which.element="node.mat", AIC.weights=AIC.weights)
     }
     if(class(hisse.results[[1]])=="hisse.geosse.states"){
-        states.internal <- ConvertManyToMultiState(hisse.results, "node.mat", AIC.weights=AIC.weights)
+        states.internal <- ConvertManyToMultiState(hisse.results, which.element="node.mat", AIC.weights=AIC.weights)
     }
     
     final.df <- data.frame(id=hisse.results[[1]]$node.mat[,1], state=states.internal, turnover=rates.tips.turnover, net.div=rates.tips.net.div, speciation=rates.tips.speciation, extinct.frac=rates.tips.extinct.fraction, extinction=rates.tips.extinction)
