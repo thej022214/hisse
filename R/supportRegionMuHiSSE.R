f338b85e787e127237ffcf09479005e15bfc6a23
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2018-06-12 13:09:42 -0500

    debugging support region

--- R/supportRegionMuHiSSE.R
+++ R/supportRegionMuHiSSE.R
@@ -39,6 +39,7 @@ SupportRegionMuHiSSE <- function(muhisse.obj, n.points=1000, scale.int=0.1, desi
     interval.results.final <- cbind(interval.results[,1], interval.results.final)
     print(interval.results.final)
     interval.results.in <- interval.results.final[which(interval.results.final[,1] - min(interval.results.final[,1])<=desired.delta),]
+    print(interval.results.in)
     ci.interval = apply(interval.results.in, 2, quantile)
     colnames(interval.results.final) <- colnames(interval.results.in) <- colnames(ci.interval) <- interval.names
     obj = NULL
