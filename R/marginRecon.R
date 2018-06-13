a793162038c1db3a1b011e80ce423c3841781b0d
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2018-06-13 10:02:00 -0500

    testing something

--- R/marginRecon.R
+++ R/marginRecon.R
@@ -543,17 +543,14 @@ MarginReconMuSSE <- function(phy, data, f, pars, hidden.states=TRUE, condition.o
     if(is.null(n.cores)){
         marginal.probs <- matrix(0, nb.node+nb.tip, nstates)
         for (i in seq(from = 1, length.out = nb.node)) {
-            print("um hi?")
             focal <- nodes[i]
             marginal.probs.tmp <- c()
             for (j in 1:nstates){
-                print(j)
                 marginal.probs.tmp <- c(marginal.probs.tmp, DownPassMuHisse(dat.tab=dat.tab, gen=gen, cache=cache, condition.on.survival=condition.on.survival, root.type=root.type, root.p=root.p, node=focal, state=j))
             }
             best.probs = max(marginal.probs.tmp)
             marginal.probs.rescaled = marginal.probs.tmp - best.probs
             marginal.probs[focal,] = exp(marginal.probs.rescaled) / sum(exp(marginal.probs.rescaled))
-            print(marginal.probs[focal,])
             if (verbose && i%%100==0) {
                 cat(paste(i, "of", nb.node, "nodes done"), "\n")
             }
