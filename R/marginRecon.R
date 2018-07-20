b68d0e600e8e4b5edb76fd9c7d8956b36dd9add3
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2018-07-20 08:27:22 -0500

    fixing bug in new recon code

--- R/marginRecon.R
+++ R/marginRecon.R
@@ -835,7 +835,7 @@ MarginReconfGeoSSE <- function(phy, data, f, pars, hidden.areas=TRUE, assume.cla
             return(c(node, marginal.probs))
         }
         node.marginals <- mclapply((nb.tip+1):(nb.tip+nb.node), NodeEval, mc.cores=n.cores)
-        
+        print("here?")
         if(hidden.areas==TRUE){
             TipEval <- function(tip){
                 dat.tab <- OrganizeData(data=data.new, phy=phy, f=f, hidden.states=hidden.areas)
