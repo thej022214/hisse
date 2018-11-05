9f889000f7045c71c0386c5cf27d8b867d73a4a8
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2018-11-05 10:42:12 -0600

    checking

--- R/hisse.R
+++ R/hisse.R
@@ -451,7 +451,8 @@ DownPass <- function(phy, cache, hidden.states, bad.likelihood=-10000000000, con
 		if(condition.on.survival == TRUE){
 			if(hidden.states == FALSE){
                 print(compD[root.node,])
-				compD[root.node,] <- compD[root.node,] / sum(root.p * c(lambda0[[1]], lambda1[[1]]) * (1 - compE[root.node,])^2)
+                #compD[root.node,] <- compD[root.node,] / sum(root.p * c(lambda0[[1]], lambda1[[1]]) * (1 - compE[root.node,])^2)
+                compD[root.node,] <- (compD[root.node,]*root.p) / (c(lambda0[[1]], lambda1[[1]]) * (1 - compE[root.node,])^2)
 				#Corrects for possibility that you have 0/0:
 				compD[root.node,which(is.na(compD[root.node,]))] = 0
 			}else{
@@ -460,7 +461,7 @@ DownPass <- function(phy, cache, hidden.states, bad.likelihood=-10000000000, con
 				compD[root.node,which(is.na(compD[root.node,]))] = 0
 			}
 		}
-		loglik <- log(sum(compD[root.node,] * root.p)) + sum(logcomp)
+		loglik <- log(sum(compD[root.node,])) + sum(logcomp)
 	}
 	if(get.phi==TRUE){
 		obj = NULL
