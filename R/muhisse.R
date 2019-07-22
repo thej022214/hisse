edd8f53c4577deae282f0fa1766d59db16473af0
Jeremy Beaulieu <jmbeauli@uaf134474.ddns.uark.edu>
2019-07-22 15:03:20 -0500

    trying to check bug

--- R/muhisse.R
+++ R/muhisse.R
@@ -599,8 +599,7 @@ FocalNodeProb <- function(cache, dat.tab, generations){
         if(!is.null(cache$node)){
             if(any(cache$node %in% generations)){
                 for(fix.index in 1:length(cache$node)){
-                    print(cache$fix.type[fix.index])
-                    if(is.na(cache$fix.type[fix.index])){
+                    if(cache$fix.type[fix.index] == "event"){
                         fixer.tmp = numeric(4)
                         fixer.tmp[cache$state[fix.index]] = 1
                         fixer = rep(fixer.tmp, 8)
@@ -654,7 +653,7 @@ GetRootProb <- function(cache, dat.tab, generations){
         if(!is.null(cache$node)){
             if(any(cache$node %in% generations)){
                 for(fix.index in 1:length(cache$node)){
-                    if(is.na(cache$fix.type[fix.index])){
+                    if(cache$fix.type[fix.index] == "event"){
                         fixer.tmp = numeric(4)
                         fixer.tmp[cache$state[fix.index]] = 1
                         fixer = rep(fixer.tmp, 8)
