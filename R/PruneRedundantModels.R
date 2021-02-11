ee4b700e58418fd511b0d801a572a3157f9123f9
Brian O'Meara <omeara.brian@gmail.com>
2021-02-10 20:59:10 -0500

    pruning redundant models

--- R/PruneRedundantModels.R
+++ R/PruneRedundantModels.R
@@ -1,9 +1,5 @@
-## Compute model weights for GeoHiSSE, HiSSE, or MiSSE models given a list of models.
-
-# BCO: DEPRECATED -- duplicates functionality in GetAICWeights, while not allowing for the possibility of AICc. Do not use. Kept here for historical reference for now
-
-GetModelWeight <- function(...){
-    models <- list(...)
+PruneRedundantModels <- function(..., precision=1e-5) {
+	models <- list(...)
     ## Check if we have a single list with all the models or the arguments are a list.
     if( !inherits(models[[1]], what = c("geohisse.fit", "hisse.fit", "misse.fit")) ){
         ## This is a list of models. Need to extract.
@@ -22,10 +18,22 @@ GetModelWeight <- function(...){
         ## Strange! Break.
         stop( "list of models need to be only HiSSE, or GeoHiSSE, or MiSSE fits." )
     }
-    mod.names <- names( models )
-    mod.AIC <- sapply(models, function(x) x$AIC )
-    delta <- mod.AIC - min( mod.AIC )
-    AICw <- exp( -0.5 * delta) / sum( exp( -0.5 * delta) )
-    names( AICw ) <- mod.names
-    return( AICw )
+	mod.nparameters <- simplify2array(lapply(lapply(models, "[[", "starting.vals"),length))
+	models <- models[order(mod.nparameters, decreasing=FALSE)]
+	mod.loglik <- simplify2array(lapply(models, "[[", "loglik"))
+	models_to_delete <- c()
+	isTrueAllEqual <- function(...) {
+		return(isTRUE(all.equal(...)))
+	}
+	if(length(models)>1) {
+		for(i in 2:(length(models))) {
+			if(any(sapply(mod.loglik[1:(i-1)], isTrueAllEqual, mod.loglik[i], tolerance=precision))) {
+				models_to_delete <- c(models_to_delete, i)
+			}
+		}
+	}
+	if(length(models_to_delete)>0) {
+		models <- models[-models_to_delete]
+	}
+    return( models )
 }
